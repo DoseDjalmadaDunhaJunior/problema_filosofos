9d28af48d9497c0c627b90fdedb48e2071ef385b
djalma <djalminhajr_cunha@hotmail.com>
2020-03-12 15:55:32 -0300

    preciso ter ideia de como fica os valores para ter certeza que esta certo, porem agora tem porcentagem no meio

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 int main() {
-        int filosofos = 5, colheres = 5,tempo = 100, porcentagem = 10, c0 = 0;
+        int filosofos = 5, colheres = 5,tempo = 100, porcentagem = 10, c0 = 0, comendo = 0;
 
         struct filosofo{
             float Tcomendo = 0;
@@ -17,130 +17,158 @@ int main() {
             bool ok = false;
             bool morreu = false;
         };
+        
+        
     srand (time(NULL));
         struct  filosofo a,b,c,d,e;
     for (int i = 0; i < tempo; i++) {
         c0 = rand() % 5 + 1;
         // true == comendo;
-        if((c0 == 1 && a.ok) && (b.colherDisponivel != 0 || e.colherDisponivel != 0)){
-            a.ok = false;
-            a.colherDisponivel = 0;
-            if(b.colherDisponivel != 0){
-                b.colherDisponivel = 0;
-                a.colherPega = 1;
-            } else{
-                e.colherDisponivel = 0;
-                a.colherPega = -1;
-            }
-        }
-
-        else if(c0 == 1 && !a.ok){
-            a.ok = true;
-            a.colherDisponivel = 1;
-            if(a.colherPega == 1){
-                b.colherDisponivel = 1;
-            }
-            else{
-                e.colherDisponivel = 1;
-            }
-            a.colherPega = 0;
-        }
-
-        else if((c0 == 2 && b.ok) && (c.colherDisponivel != 0 || a.colherDisponivel != 0)){
-            b.ok = false;
-            b.colherDisponivel = 0;
-            if(a.colherDisponivel != 0){
+        if(comendo < 2) {
+            if ((c0 == 1 && a.ok) && (b.colherDisponivel != 0 || e.colherDisponivel != 0)) {
+                a.ok = false;
                 a.colherDisponivel = 0;
-                b.colherPega = -1;
-            }
-            else{
-                c.colherDisponivel = 0;
-                b.colherPega = 1;
-            }
-        }
-
-        else if(c0 == 2 && !b.ok){
-            b.ok = true;
-            b.colherDisponivel = 1;
-            if(b.colherPega == -1){
+                comendo++;
+                if (b.colherDisponivel != 0) {
+                    b.colherDisponivel = 0;
+                    a.colherPega = 1;
+                } else {
+                    e.colherDisponivel = 0;
+                    a.colherPega = -1;
+                }
+            } else if (c0 == 1 && !a.ok) {
+                a.ok = true;
                 a.colherDisponivel = 1;
-            }
-            else{
-                c.colherDisponivel = 1;
-            }
-            b.colherPega = 0;
-        }
-
-        else if((c0 == 3 && c.ok) && (d.colherDisponivel != 0 || b.colherDisponivel != 0)){
-            c.ok = false;
-            c.colherDisponivel = 0;
-            if(d.colherDisponivel != 0){
-                d.colherDisponivel = 0;
-                c.colherPega = 1;
-            }
-            else{
+                
+                if (a.colherPega == 1) {
+                    b.colherDisponivel = 1;
+                } else {
+                    e.colherDisponivel = 1;
+                }
+                a.colherPega = 0;
+            } else if ((c0 == 2 && b.ok) && (c.colherDisponivel != 0 || a.colherDisponivel != 0)) {
+                b.ok = false;
                 b.colherDisponivel = 0;
-                c.colherPega = -1;
-            }
-        }
-
-        else if(c0 == 3 && !c.ok){
-            c.ok = true;
-            c.colherDisponivel = 1;
-            if(c.colherPega == -1){
+                comendo++;
+                if (a.colherDisponivel != 0) {
+                    a.colherDisponivel = 0;
+                    b.colherPega = -1;
+                } else {
+                    c.colherDisponivel = 0;
+                    b.colherPega = 1;
+                }
+            } else if (c0 == 2 && !b.ok) {
+                b.ok = true;
                 b.colherDisponivel = 1;
-            }
-            else{
-                d.colherDisponivel = 1;
-            }
-            c.colherPega = 0;
-        }
-
-        else if((c0 == 4 && d.ok) && (c.colherDisponivel != 0 || e.colherDisponivel !=0)){
-            d.ok = false;
-            d.colherDisponivel = 0;
-            if(c.colherDisponivel !=0){
+                
+                if (b.colherPega == -1) {
+                    a.colherDisponivel = 1;
+                } else {
+                    c.colherDisponivel = 1;
+                }
+                b.colherPega = 0;
+            } else if ((c0 == 3 && c.ok) && (d.colherDisponivel != 0 || b.colherDisponivel != 0)) {
+                c.ok = false;
                 c.colherDisponivel = 0;
-                d.colherPega = -1;
-            }
-            else{
-                e.colherDisponivel = 0;
-                d.colherPega = 1;
-            }
-        }
-        else if(c0 == 4 && !d.ok){
-            d.ok = true;
-            d.colherDisponivel = 1;
-            if(d.colherPega == 1){
-                e.colherDisponivel = 1;
-            }
-            else{
+                comendo++;
+                if (d.colherDisponivel != 0) {
+                    d.colherDisponivel = 0;
+                    c.colherPega = 1;
+                } else {
+                    b.colherDisponivel = 0;
+                    c.colherPega = -1;
+                }
+            } else if (c0 == 3 && !c.ok) {
+                c.ok = true;
                 c.colherDisponivel = 1;
-            }
-            d.colherPega = 0;
-        }
-        else if((c0 == 5 && e.ok) && (a.colherDisponivel != 0 || d.colherDisponivel != 0)){
-            e.ok = false;
-            e.colherDisponivel = 0;
-            if(a.colherDisponivel != 0){
-                a.colherDisponivel = 0;
-                e.colherPega = 1;
-            }
-            else{
+                
+                if (c.colherPega == -1) {
+                    b.colherDisponivel = 1;
+                } else {
+                    d.colherDisponivel = 1;
+                }
+                c.colherPega = 0;
+            } else if ((c0 == 4 && d.ok) && (c.colherDisponivel != 0 || e.colherDisponivel != 0)) {
+                d.ok = false;
                 d.colherDisponivel = 0;
-                e.colherPega = -1;
+                comendo++;
+                if (c.colherDisponivel != 0) {
+                    c.colherDisponivel = 0;
+                    d.colherPega = -1;
+                } else {
+                    e.colherDisponivel = 0;
+                    d.colherPega = 1;
+                }
+            } else if (c0 == 4 && !d.ok) {
+                d.ok = true;
+                d.colherDisponivel = 1;
+                
+                if (d.colherPega == 1) {
+                    e.colherDisponivel = 1;
+                } else {
+                    c.colherDisponivel = 1;
+                }
+                d.colherPega = 0;
+            } else if ((c0 == 5 && e.ok) && (a.colherDisponivel != 0 || d.colherDisponivel != 0)) {
+                e.ok = false;
+                e.colherDisponivel = 0;
+                comendo++;
+                if (a.colherDisponivel != 0) {
+                    a.colherDisponivel = 0;
+                    e.colherPega = 1;
+                } else {
+                    d.colherDisponivel = 0;
+                    e.colherPega = -1;
+                }
+            } else if (c0 == 5 && !e.ok) {
+                e.ok = true;
+                e.colherDisponivel = 1;
+                
+                if (e.colherPega = -1) {
+                    d.colherDisponivel = 1;
+                } else {
+                    a.colherDisponivel = 1;
+                }
+                d.colherPega = 0;
             }
         }
-        else if(c0 == 5 && !e.ok){
-            e.ok = true;
-            e.colherDisponivel = 1;
-            if(e.colherPega = -1){
-                d.colherDisponivel = 1;
+        
+        else{
+            if(!a.ok){
+                a.Tcomendo++;
             }
-            else{
-            	a.colherDisponivel = 1;
-			}
-			d.colherPega = 0;
+            if(a.Tcomendo >= porcentagem){
+                comendo--;
+                a.Tcomendo = 0;
+            }
+            if(!b.ok){
+                b.Tcomendo++;
+            }
+            if(b.Tcomendo >= porcentagem){
+                comendo--;
+                b.Tcomendo = 0;
+            }
+            if(!c.ok){
+                c.Tcomendo++;
+            }
+            if(c.Tcomendo >= porcentagem){
+                comendo--;
+                c.Tcomendo = 0;
+            }
+            if(!d.ok){
+                d.Tcomendo++;
+            }
+            if(d.Tcomendo >= porcentagem){
+                comendo--;
+                d.Tcomendo = 0;
+            }
+            if(!e.ok){
+                e.Tcomendo++;
+            }
+            if(e.Tcomendo >= porcentagem){
+                comendo--;
+                e.Tcomendo = 0;
+            }
         }
         if(a.ok){
             a.Tpensando++;
